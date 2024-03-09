1f02b4461af28113849914dee7a3dee179acd7fb
Raquel <raquelhermawan@gmail.com>
2024-03-10 01:22:29 +0700

    update

--- app.js
+++ app.js
@@ -29,17 +29,13 @@ let slideIndex = 0;
 function showSlides() {
 	const slides = document.querySelectorAll('.plants-item');
 	const dots = document.querySelectorAll('.dot');
-	// Hide all slides
 	for (let i = 0; i < slides.length; i++) {
 	  slides[i].style.display = 'none';
 	}
-	// Remove all active dots
 	for (let i = 0; i < dots.length; i++) {
 	  dots[i].classList.remove('active');
 	}
-	// Show current slide
 	slides[slideIndex].style.display = 'flex';
-	// Add active class to current dot
 	dots[slideIndex].classList.add('active');
   }
   
@@ -63,13 +59,12 @@ function showSlides() {
 	showSlides();
   }
   
-  // Dynamically create dots based on the number of slides
   const slides = document.querySelectorAll('.plants-item');
   const dotsContainer = document.querySelector('.dot-container');
   for (let i = 0; i < slides.length; i++) {
 	const dot = document.createElement('span');
 	dot.classList.add('dot');
-	dot.setAttribute('data-slide-index', i); // Set custom attribute to store slide index
+	dot.setAttribute('data-slide-index', i);
 	dot.addEventListener('click', function() {
 	  slideIndex = parseInt(this.getAttribute('data-slide-index'));
 	  showSlides();
@@ -77,6 +72,5 @@ function showSlides() {
 	dotsContainer.appendChild(dot);
   }
   
-  // Show first slide initially
   showSlides();
   
